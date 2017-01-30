cd55e33e60e9aee5b678aafb903e174b2fc2ae55
Menno Godeke <mgodeke1994@gmail.com>
2017-01-30 19:27:23 +0100

    scaffolded labels

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :labels
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
 
---
+++ db/migrate/20170130181915_create_labels.rb
@@ -0,0 +1,10 @@
+class CreateLabels < ActiveRecord::Migration[5.0]
+  def change
+    create_table :labels do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
